06fe0c44a09f355b30b248a453e2a2da10762e3b
Sunkara Mohammed Hussain <smdh4321@gmail.com>
2025-07-07 11:35:24 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Keypad from './Keypad';
 
 function App() {
+  let [input,setInput] = useState("");
+
+  function handleClick(value){
+    setInput(input+value)
+  }
+
+  function calculate() {
+  if (!input) {
+    return; // Do nothing if input is empty
+  }
+
+  try {
+    const output = eval(input);
+    setInput(output.toString());
+  } catch (error) {
+    setInput("Error"); // Handle invalid expressions
+  }
+}
+
+  function handleClear(){
+    setInput("")
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h1>Calculator App</h1>
+      <div className='calculator'>
+        <input type='text' className='output' value={input}></input>
+        <Keypad handleClick={handleClick} handleClear={handleClear} calculate={calculate}/>
+      </div>
     </div>
   );
 }
 
 export default App;
+
